10b561a5594652a5e3584bd89bd44cf18784a455
meszolym <62993044+meszolym@users.noreply.github.com>
2023-11-19 16:05:57 +0100

    asd

--- HKDXX6_HFT_2023241.Client/Program.cs
+++ HKDXX6_HFT_2023241.Client/Program.cs
@@ -4,9 +4,10 @@ namespace HKDXX6_HFT_2023241.Client
 {
     internal class Program
     {
+        static RestService rest;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            rest = new("http://localhost:24150");
         }
     }
 }

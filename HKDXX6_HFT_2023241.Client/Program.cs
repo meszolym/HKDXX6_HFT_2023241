fbea368d37fcf349eac75dbbc6907b899fd9c576
meszolym <62993044+meszolym@users.noreply.github.com>
2023-11-19 16:16:39 +0100

    Revert "Revert "Copied RestService from nikprog.hu""
    This reverts commit 9f36697e566e079589ccb3d1678c7030c4f328a1.

--- HKDXX6_HFT_2023241.Client/Program.cs
+++ HKDXX6_HFT_2023241.Client/Program.cs
@@ -4,10 +4,9 @@ namespace HKDXX6_HFT_2023241.Client
 {
     internal class Program
     {
-        static RestService rest;
         static void Main(string[] args)
         {
-            rest = new("http://localhost:24150");
+            Console.WriteLine("Hello World!");
         }
     }
 }

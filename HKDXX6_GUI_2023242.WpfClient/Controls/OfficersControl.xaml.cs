480714ca9312468ae1afdbe82bf2b37138c0c2e0
meszolym <62993044+meszolym@users.noreply.github.com>
2024-03-04 21:41:39 +0100

    Made the base WPF window and controls for the UI

---
+++ HKDXX6_GUI_2023242.WpfClient/Controls/OfficersControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HKDXX6_GUI_2023242.WpfClient.Controls
+{
+    /// <summary>
+    /// Interaction logic for OfficersControl.xaml
+    /// </summary>
+    public partial class OfficersControl : UserControl
+    {
+        public OfficersControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HKDXX6_GUI_2023242.WpfClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using HKDXX6_GUI_2023242.WpfClient.Controls;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+
+
+namespace HKDXX6_GUI_2023242.WpfClient.ViewModels
+{
+    public class MainWindowViewModel : ObservableRecipient
+    {
+        WelcomeControl welcomeControl;
+        PrecinctsControl precinctsControl;
+        OfficersControl officersControl;
+        CasesControl casesControl;
+        StatisticsControl statisticsControl;
+
+        UserControl currentControl;
+        public UserControl CurrentControl
+        {
+            get
+            {
+                return currentControl;
+            }
+            set
+            {
+                SetProperty(ref currentControl, value);
+            }
+        }
+
+        public ICommand NavToPrecincts { get; set; }
+        public ICommand NavToCases { get; set; }
+        public ICommand NavToOfficers { get; set; }
+        public ICommand NavToStatistics { get; set; }
+
+        public MainWindowViewModel()
+        { 
+            welcomeControl = new WelcomeControl();
+            precinctsControl = new PrecinctsControl();
+            officersControl = new OfficersControl();
+            casesControl = new CasesControl();
+            statisticsControl = new StatisticsControl();
+            
+            CurrentControl = welcomeControl;
+            
+            NavToPrecincts = new RelayCommand(() =>
+            {
+                CurrentControl = precinctsControl;
+            });
+
+            NavToOfficers = new RelayCommand(() =>
+            {
+                CurrentControl = officersControl;
+            });
+
+            NavToCases = new RelayCommand(() =>
+            {
+                CurrentControl = casesControl;
+            });
+
+            NavToStatistics = new RelayCommand(() =>
+            {
+                CurrentControl = statisticsControl;
+            });
+        }
+    }
+}

ad638d31f42ac8038c8b90d65d96d0b2972db827
meszolym <62993044+meszolym@users.noreply.github.com>
2024-03-15 20:49:16 +0100

    Adding and deleting in precincts is ok.

---
+++ HKDXX6_GUI_2023242.JsClient/wwwroot/Code/precinctScript.js
@@ -0,0 +1,92 @@
+﻿let precincts = [];
+
+getdata();
+
+async function getdata() {
+    await fetch('http://localhost:33410/precinct')
+        .then(x => x.json())
+        .then(y => {
+            precincts = y
+            display();
+        });
+}
+function display() {
+    document.getElementById('tableBody').innerHTML = '';
+
+    precincts.forEach(p => {
+        document.getElementById('tableBody').innerHTML +=
+            `<tr>
+                <td>${p.id}</td>
+                <td>${p.address}</td>
+                <td>
+                    <button class="pure-button" type="button" onclick=remove('${p.id}')>Delete</button>
+                    <button class="pure-button" type="button" onclick=edit('${p}')>Edit</button>
+                </td>
+            </tr>`
+    });
+
+}
+
+function add() {
+    let idInput = document.getElementById('idInput').value;
+    let addressInput = document.getElementById('addressInput').value;
+
+    fetch('http://localhost:33410/precinct', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', },
+        body: JSON.stringify(
+            {
+                id: idInput,
+                address: addressInput
+            }
+        )
+    }).then(response => {
+        if (!response.ok) {
+            return response.json();
+        }
+        else {
+            getdata();
+            display();
+        }
+    }).then(data => {
+        if (data != undefined) {
+            console.log(data);
+            if (data.msg != undefined) {
+                throw new Error(data.msg);
+            }
+        }
+    }).catch(error => {
+        console.error(error);
+        alert(error.message);
+    });
+
+    document.getElementById('idInput').value = "";
+    document.getElementById('addressInput').value = "";
+
+}
+function remove(id) {
+    fetch('http://localhost:33410/precinct/' + id, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json', },
+        body: null
+    }).then(response => {
+        if (!response.ok) {
+            return response.json();
+        }
+        else {
+            getdata();
+            display();
+        }
+    }).then(data => {
+        if (data != undefined) {
+            console.log(data);
+            if (data.msg != undefined) {
+                throw new Error(data.msg);
+            }
+        }
+    }).catch(error => {
+        console.error(error);
+        alert(error.message);
+    });
+    
+}

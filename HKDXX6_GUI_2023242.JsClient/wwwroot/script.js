31a18f828c3ae2d8214195ec6ac48c5346d7d9e8
meszolym <62993044+meszolym@users.noreply.github.com>
2024-03-16 15:52:40 +0100

    made all in one page, have to transfer.

---
+++ HKDXX6_GUI_2023242.JsClient/wwwroot/script.js
@@ -0,0 +1,28 @@
+﻿
+function showMain() {
+    document.getElementById('welcomeDiv').style.display = 'block';
+    document.getElementById('precinctsDiv').style.display = 'none';
+    document.getElementById('officersDiv').style.display = 'none';
+    document.getElementById('casesDiv').style.display = 'none';
+}
+
+function showPrecincts() {
+    document.getElementById('welcomeDiv').style.display = 'none';
+    document.getElementById('precinctsDiv').style.display = 'block';
+    document.getElementById('officersDiv').style.display = 'none';
+    document.getElementById('casesDiv').style.display = 'none';
+}
+
+function showOfficers() {
+    document.getElementById('welcomeDiv').style.display = 'none';
+    document.getElementById('precinctsDiv').style.display = 'none';
+    document.getElementById('officersDiv').style.display = 'block';
+    document.getElementById('casesDiv').style.display = 'none';
+}
+
+function showCases() {
+    document.getElementById('welcomeDiv').style.display = 'none';
+    document.getElementById('precinctsDiv').style.display = 'none';
+    document.getElementById('officersDiv').style.display = 'none';
+    document.getElementById('casesDiv').style.display = 'block';
+}
